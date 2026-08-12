b1409177d44bafc4ec0a2359ebb5235a0a0972b4
YeffryF <yeffry.fermin.ttp@gmail.com>
2026-08-12 11:28:33 -0400

    style: update all notes layout

--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react"
 import './Notes.css'
 import { Link } from "react-router"
+import { CgFileDocument } from "react-icons/cg";
+import { CiStar } from "react-icons/ci";
+
 function Notes() {
     const [notes, setNotes] = useState(null)
 
@@ -19,23 +22,40 @@ function Notes() {
 
     return (
         <section>
-            <div>Search Section</div>
-            <input type="text" placeholder="Find note"/>
+            <div className="content-wrapper">
+                <input type="text" className="note-search" placeholder="Find note"/>
+            </div>
             <hr />
-            <div className="note-container">
+            <div className="note-container content-wrapper">
                 {notes && (
                     notes.map((note) => {
                         return (
-                            <Link to={`/note/${note.id}/view`}>
+                            <Link to={`/note/${note.id}/view`} key={note.id} className="note-link">
                                 <div className="note-card">
-                                    <h3>{note.title ? note.title : 'Empty Title'}</h3>
+                                    <div className="note-header">
+                                        <div className="note-icon">
+                                            <CgFileDocument size={25}/>
+                                        </div>
+                                        <div className="note-title">
+                                            <h3>{note.title ? note.title : 'Untitled'}</h3>
+                                        </div>
+                                        <div className="note-favorite">
+                                            <CiStar size={22} />
+                                        </div>
+                                    </div>
+                                    <div className="note-body">
+                                        <p>{note.description}</p>
+                                    </div>
+                                    <div className="note-tags">
+                                        <span className="note-tag">#sample tag</span>
+                                        <span className="note-tag">#sample tag #2</span>
+                                    </div>
                                 </div>
                             </Link>
                         )
                     })
                 )}
             </div>
-            
         </section>
     )
 }
